7c5ad6340701b757dd0a07d5622e5dbaf76c1b99
indmind <sattanationmail@gmail.com>
2018-02-16 23:59:14 +0700

    add limitedEval for security

--- src/command/calc.js
+++ src/command/calc.js
@@ -2,6 +2,32 @@ import math from 'mathjs'
 
 export default class Calc {
     constructor() {
+        this.limitedEval = math.eval
+
+        math.import(
+            {
+                import: function() {
+                    throw new Error('Function import is disabled')
+                },
+                createUnit: function() {
+                    throw new Error('Function createUnit is disabled')
+                },
+                eval: function() {
+                    throw new Error('Function eval is disabled')
+                },
+                parse: function() {
+                    throw new Error('Function parse is disabled')
+                },
+                simplify: function() {
+                    throw new Error('Function simplify is disabled')
+                },
+                derivative: function() {
+                    throw new Error('Function derivative is disabled')
+                }
+            },
+            { override: true }
+        )
+
         this.VARIABLE = {
             emptyExpression:
                 ':angry: are you kidding me? what should I calculate?',
@@ -24,7 +50,9 @@ export default class Calc {
         let result
 
         try {
-            result = `${this.randomText} ${math.eval(expression).toString()}`
+            result = `${this.randomText} ${math
+                .limitedEval(expression)
+                .toString()}`
         } catch (error) {
             result = this.VARIABLE.calculationError
         }
