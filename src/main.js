507d1bc55e8feca126032e3068662a87b6b842db
indmind <sattanationmail@gmail.com>
2018-02-16 23:24:24 +0700

    change overall structure

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-require(`./vendor/${process.env.STATUS == 'production' ? 'main' : 'cli'}`)
+if (process.env.STATUS == 'debug') {
+    console.log('Running in cli interface')
+    require('./vendor/cli')
+} else if (process.env.HEROKU) {
+    console.log('Runing on production server')
+    require('./vendor/http')
+}
+
+if (process.env.DISCORD_TOKEN) {
+    console.log('DISCORD token detected! include discord')
+    require('./vendor/discord')
+}
+
+if (process.env.TELEGRAM_TOKEN) {
+    console.log('TELEGRAM token detected! include telegram')
+    require('./vendor/telegram')
+}
