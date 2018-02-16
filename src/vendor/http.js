c006c0672770a75fac662b17f4fbbb5c384a7635
indmind <sattanationmail@gmail.com>
2018-02-17 01:04:21 +0700

    add fork me ribbon

--- src/vendor/http.js
+++ src/vendor/http.js
@@ -1,10 +1,16 @@
 import http from 'http'
+import fs from 'fs'
+
+const index = __dirname + '/../../view/index.html'
+
+const indexHtml = fs.readFileSync(index, 'utf8').toString()
 
 http
     .createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(
-            `Hello! I am Rin, I am a simple multi-purpose bot, available in <a href="http://t.me/rin_a_bot">Telegram</a> and <a href="https://discordapp.com/oauth2/authorize?&client_id=412976772150329354&scope=bot&permissions=0">Discord</a>`
-        )
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write(indexHtml)
+        res.end()
     })
     .listen(process.env.PORT || 8080)
