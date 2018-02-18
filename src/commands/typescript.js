8dbf694fddfbfed1cb9be8a6d1c419e2bcdcc9b4
indmind <sattanationmail@gmail.com>
2018-02-18 14:52:03 +0700

    add typescript compiler

---
+++ src/commands/typescript.js
@@ -0,0 +1,33 @@
+import Rin from '../core/rin'
+import tsc from 'typescript-compiler'
+
+export default class Typescript {
+    constructor() {
+        this.INFO = {
+            command: 'typescript',
+            description: 'compile typescript to javascript'
+        }
+        this.VARIABLE = {
+            codeEmpty: 'empty code!',
+            unknownEror: 'sorry, an unknown error occurred'
+        }
+    }
+
+    async handle(command) {
+        const ts = command.join(' ')
+
+        if (Rin.isEmpty(ts)) return this.VARIABLE.codeEmpty
+
+        try {
+            const result = tsc.compileString(ts, null, null, e => {
+                if (e) throw e.formattedMessage
+            })
+
+            if (Rin.isEmpty(result)) return this.VARIABLE.unknownEror
+
+            return result
+        } catch (err) {
+            return err.message || err
+        }
+    }
+}
