154129b35f2e8124853d3cb4f7933ce5ae356879
indmind <sattanationmail@gmail.com>
2018-02-22 22:09:18 +0700

    add bot status and typing info

--- src/vendor/discord.js
+++ src/vendor/discord.js
@@ -7,23 +7,31 @@ const rin = new Rin()
 
 client.on('ready', () => {
     Rin.log.info(`Logged in as ${client.user.tag}!`)
+
+    client.user.setPresence({
+        status: 'online',
+        game: { name: 'Half Lie' }
+    })
 })
 
 client.on('message', async ctx => {
     const message = ctx.content
     const args = message.split(' ')
+    const username = ctx.author.username
 
     if (!(args[0] == client.user.username)) return
 
+    ctx.channel.startTyping()
+
     Rin.log.info(
-        `[DISCORD]${ctx.author.username}(${ctx.author.id}): ${Rin.standarize(
-            message
-        )}`
+        `[DISCORD]${username}(${ctx.author.id}): ${Rin.standarize(message)}`
     )
 
     const subcmd = args.slice(1).join(' ')
 
     const result = await rin.handle(subcmd, { vendor: 'discord' })
+
+    ctx.channel.stopTyping(true)
 
     if (result.length > 2000) {
         ctx.reply("Sorry It's too big to send :disappointed:")
