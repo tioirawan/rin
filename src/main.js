807965af457dec491f46d1e21ce1901437a6b573
indmind <sattanationmail@gmail.com>
2018-03-02 08:26:49 +0700

    error console eslint

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import logger from 'winston'
+/* eslint-disable no-console */
 
 if (process.env.STATUS == 'debug') {
-    logger.info('Running in cli interface')
+    console.log('Running in cli interface')
     require('./vendor/cli')
 } else if (process.env.HEROKU) {
-    logger.info('Runing on production server')
+    console.log('Runing on production server')
     require('./vendor/http')
 }
 
 if (process.env.DISCORD_TOKEN) {
-    logger.info('DISCORD token detected! include discord')
+    console.log('DISCORD token detected! include discord')
     require('./vendor/discord')
 }
 
 if (process.env.TELEGRAM_TOKEN) {
-    logger.info('TELEGRAM token detected! include telegram')
+    console.log('TELEGRAM token detected! include telegram')
     require('./vendor/telegram')
 }
